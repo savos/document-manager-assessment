43534e573317ae77edee88adea6236db45246689
savo-s <savo.freelance@gmail.com>
2025-09-12 07:09:05 +0200

    Added proxy to vite server

--- client/doc-manager/vite.config.ts
+++ client/doc-manager/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      '/api': 'http://localhost:8001',
+    },
     port: 5173,
   },
 })
