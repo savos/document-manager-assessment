2e67aaa6e9956a1250bc696d4cca1d398d73f336
savos <savos@users.noreply.github.com>
2025-09-13 05:23:12 +0200

    Fix settings module path and relax argon2 requirement

--- manage.py
+++ manage.py
@@ -4,7 +4,9 @@ import sys
 from pathlib import Path
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", "propylon_document_manager.site.settings.local"
+    )
 
     try:
         from django.core.management import execute_from_command_line
